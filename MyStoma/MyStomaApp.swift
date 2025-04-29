da62bf48fd5e9a772e94128fe522b2d1ee459607
azu-any <azuany.mila@gmail.com>
2025-04-29 13:24:06 +0200

    Initial Commit

---
+++ MyStoma/MyStomaApp.swift
@@ -0,0 +1,33 @@
+//
+//  MyStomaApp.swift
+//  MyStoma
+//
+//  Created by Azuany Mila Cerón on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MyStomaApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ MyStomaTests/MyStomaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyStomaTests.swift
+//  MyStomaTests
+//
+//  Created by Azuany Mila Cerón on 29/04/25.
+//
+
+import Testing
+@testable import MyStoma
+
+struct MyStomaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
