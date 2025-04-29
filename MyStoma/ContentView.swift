243380d67f04a18f21ba5eb88bf57a9441210c0c
azu-any <azuany.mila@gmail.com>
2025-04-29 13:57:23 +0200

    Added to title to content view

--- MyStoma/ContentView.swift
+++ MyStoma/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
                 .padding(.bottom, 50)
 
             Text("Hello, world!")
+            Text("MyStoma")
 
             ToggleImmersiveSpaceButton()
         }
