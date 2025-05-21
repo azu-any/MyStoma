892d759306408d6189758dbf76d16d916739c9ab
azu-any <azuany.mila@gmail.com>
2025-05-21 17:23:50 +0200

    new scenes

--- MyStoma/Helpers/Constants.swift
+++ MyStoma/Helpers/Constants.swift
@@ -9,9 +9,10 @@ import Foundation
 import simd
 
 let stomaTargetPosition = SIMD3<Float>(0.96, 1.025, -1.32)
-let wasteBagTargetPosition = SIMD3<Float>(0, 0.8, -1.5)
-let bottleTargetPosition = SIMD3<Float>(-0.3, 0.8, -1.5)
-let cleanBagPosition = SIMD3<Float>(0.25, 0, -1.6)
+let wasteBagTargetPosition = SIMD3<Float>(0.15, 0.88, -1.5)
+let bottleTargetPosition = SIMD3<Float>(-0.25, 0.8, -1.5)
+
+let cleanBagPosition = SIMD3<Float>(0.25, 0.85, -1.6)
 
 
 let tablePosition = SIMD3<Float>(0, 0, -1.50)
--- MyStoma/MyStomaApp.swift
+++ MyStoma/MyStomaApp.swift
@@ -56,5 +56,112 @@ struct MyStomaApp: App {
         }
         .immersionStyle(selection: .constant(.mixed), in: .mixed)
         #endif
+        
+        #if os(visionOS)
+        ImmersiveSpace(id: ColostomySpace.third.id) {
+            ColostomyThirdView()
+                .environment(appModel)
+                .environmentObject(viewModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+        #endif
+        
+        #if os(visionOS)
+        ImmersiveSpace(id: ColostomySpace.fourth.id) {
+            ColostomyFourthView()
+                .environment(appModel)
+                .environmentObject(viewModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+        #endif
+        
+        #if os(visionOS)
+        ImmersiveSpace(id: ColostomySpace.fifth.id) {
+            ColostomyFifthView()
+                .environment(appModel)
+                .environmentObject(viewModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+        #endif
+        
+        #if os(visionOS)
+        ImmersiveSpace(id: ColostomySpace.sixth.id) {
+            ColostomySixthView()
+                .environment(appModel)
+                .environmentObject(viewModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+        #endif
+        
+        #if os(visionOS)
+        ImmersiveSpace(id: ColostomySpace.seventh.id) {
+            ColostomySeventhView()
+                .environment(appModel)
+                .environmentObject(viewModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+        #endif
+        
+        #if os(visionOS)
+        ImmersiveSpace(id: ColostomySpace.eighth.id) {
+            ColostomyEighthView()
+                .environment(appModel)
+                .environmentObject(viewModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+        #endif
+        
+        #if os(visionOS)
+        ImmersiveSpace(id: ColostomySpace.ninth.id) {
+            ColostomyNinthView()
+                .environment(appModel)
+                .environmentObject(viewModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+        #endif
+        
+        
      }
 }
