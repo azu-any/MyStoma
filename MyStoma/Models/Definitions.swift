e0ccd34dd8613adba280cca1001ece74f8438845
azu-any <azuany.mila@gmail.com>
2025-05-26 15:11:05 +0200

    vision pro

---
+++ MyStoma/Models/Definitions.swift
@@ -0,0 +1,12 @@
+//
+//  Definitions.swift
+//  MyStoma
+//
+//  Created by Azuany Mila Cerón on 23/05/25.
+//
+
+import Foundation
+
+let definitions: [String: String] = [
+        "plaque": "The baseplate (or wafer) is the adhesive part placed around the stoma to secure the bag. It protects the skin and ensures a leak-proof seal.\nIt’s made from hydrocolloid materials, which are skin-friendly, moisture-absorbing, and conform to uneven surfaces. Some also contain flexible or silicone components for added comfort and durability.",
+    ]
--- MyStoma/Views/Components/ChatBubble.swift
+++ MyStoma/Views/Components/ChatBubble.swift
@@ -9,6 +9,7 @@ struct ChatBotOverlay: View {
     @Binding var showEnd: Bool
     @Binding var showInventory: Bool
     
+    
     var body: some View {
         
         VStack {
@@ -16,11 +17,19 @@ struct ChatBotOverlay: View {
             VStack(alignment: .center) {
                 let dialogue = viewModel.ostomy.steps[viewModel.currentStepIndex].dialogues[viewModel.currentDialogueIndex]
                 
-                Text(dialogue)
-                //TypingTextView(fullText: dialogue, trigger: viewModel.currentDialogueIndex)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                    .transition(.slide)
+                // Text(dialogue)
+                TextViewWithPopovers(
+                    fullText: dialogue
+                        )
+                    //.multilineTextAlignment(.center)
+                    //.padding()
+                    /*.popover(isPresented: $showPopover, attachmentAnchor: .point(.bottom), arrowEdge: .bottom) {
+                        if let word = selectedWord, let definition = definitions[word.lowercased()] {
+                            Text(definition)
+                                .padding()
+                                .frame(maxWidth: 300)
+                        }
+                    }*/
             }
             .frame(maxWidth: 500)
             .font(.body)
@@ -98,3 +107,145 @@ struct ChatBotOverlay: View {
         .environmentObject(OstomyViewModel(ostomy: loadOstomyFromBundle() ?? defaultOstomy)
         )
 }
+
+
+struct TextViewWithPopovers: View {
+    let fullText: String
+     let word = "plaque"
+
+    @State var selectedWord: String?
+    @State var showPopover: Bool = false
+    @State var hasWord: Bool = false
+    @State private var attributedText = AttributedString("")
+
+    var body: some View {
+            Text(attributedText)
+            .padding()
+                .onTapGesture {
+                    if hasWord {
+                        showPopover = true
+                    }
+                }
+                .onAppear{
+                    setupAttributedText()
+                }
+                .onChange(of: fullText) {
+                    setupAttributedText()
+                }
+                .popover(isPresented: $showPopover) {
+                    Text("Definition of '\(word)' goes here.")
+                        .padding()
+                }
+        }
+    
+
+        private func setupAttributedText() {
+            var attributed = AttributedString(fullText)
+            let cleanedWord = word.trimmingCharacters(in: .punctuationCharacters.union(.whitespacesAndNewlines))
+
+            if let range = attributed.range(of: cleanedWord, options: .caseInsensitive) {
+                attributed[range].foregroundColor = .accentColor
+                attributed[range].font = .body.bold()
+                hasWord = true
+            } else {
+                hasWord = false
+            }
+
+            attributedText = attributed
+        }
+    /*
+    var body: some View {
+            
+        let attributed: AttributedString = {
+                    var attr = AttributedString(fullText)
+                    let cleanedWord = word.trimmingCharacters(in: .punctuationCharacters.union(.whitespacesAndNewlines))
+                    
+                    if let range = attr.range(of: cleanedWord, options: .caseInsensitive) {
+                        hasWord = true
+                        attr[range].foregroundColor = .accentColor
+                        attr[range].font = .body.bold()
+                    } /*else {
+                        hasWord = false
+                    }*/
+                    print(hasWord)
+                    return attr
+                }()
+                
+                Text(attributed)
+                    .onTapGesture {
+                        if hasWord {
+                            showPopover = true
+                        }
+                    }
+                    .popover(isPresented: $showPopover) {
+                        Text("Definition of '\(word)' goes here.")
+                            .padding()
+                    }*/
+        /*VStack {
+            var attributed = AttributedString(fullText)
+
+            if let range = attributed.range(of: word, options: .caseInsensitive) {
+                if !hasWord {
+                    hasWord = true
+                    print(hasWord)
+                }
+                attributed[range].foregroundColor = .accentColor
+                attributed[range].font = .body.bold()
+                
+            }
+    
+            return Text(attributed)
+                .onTapGesture {
+                    showPopover = true
+                }
+            }
+            .padding()
+            .popover(isPresented: $showPopover, content: {
+                // El contenido del popover, en este caso, una definición de la palabra
+                Text("La palabra \(word) significa...")
+                    .padding()
+                    .frame(width: 200, height: 100)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        showPopover = false
+                    }
+            })
+            .disabled(!hasWord)
+        */
+        /*VStack {
+            Text(fullText)
+                .foregroundStyle(.clear)
+            //.multilineTextAlignment(.center) // start with an empty view
+                .overlay(alignment: .center) {
+                    HStack(spacing: 4) {
+                        ForEach(Array(words), id: \.self) { word in
+                            let trimmed = word.trimmingCharacters(in: .punctuationCharacters)
+                            if definitions.keys.contains(trimmed.lowercased()) {
+                                Button {
+                                    selectedWord = trimmed
+                                    showPopover = true
+                                } label: {
+                                    Text(String(word))
+                                        .foregroundColor(.blue)
+                                        .underline()
+                                }
+                                .popover(isPresented: $showPopover, attachmentAnchor: .point(.bottom), arrowEdge: .bottom) {
+                                    if let word = selectedWord, let definition = definitions[word.lowercased()] {
+                                        Text(definition)
+                                            .padding()
+                                            .frame(maxWidth: 300)
+                                    }
+                                }
+                            } else {
+                                Text(word)//String(word))
+                                    .foregroundStyle(.black)
+                            }
+                        }
+                    }
+                    //.multilineTextAlignment(.center)
+                }
+        }
+    }*/
+}
+
